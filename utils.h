3c75cc410f514e2dafded5a36e21b4e4d566ef1c
Daydream0929 <2352869723@qq.com>
2023-08-31 16:31:48 +0800

    add some utils functions

---
+++ utils.h
@@ -0,0 +1,19 @@
+// Utility functions for socket servers in C.
+// This code is in the public domain.
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+// Reports a peer connection to stdout. sa is the data populated by a successful
+// accept() call.
+void report_peer_connected(const struct sockaddr_in* sa, socklen_t salen);
+
+// Creates a bound and listening INET socket on the given port number. Returns
+// the socket fd when successful; dies in case of errors.
+int listen_inet_socket(int portnum);
+
+#endif /* UTILS_H */
